778c45ec1cb14cf1ae8bc0180658d98b0b76cbcd
Nia <kikyoporter14@yahoo.com>
2016-04-18 19:43:23 -0400

    Fire balls and more waves!!!!!!!!

---
+++ PolyRunner/Assets/Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	GameObject[] rams;
+	// Use this for initialization
+	void Start () {
+		rams = new GameObject[4];
+		rams[0] = GameObject.Find("satansram");
+		rams[1] = GameObject.Find("satansram (1)");
+		rams[2] = GameObject.Find("satansram (2)");
+		rams[3] = GameObject.Find("satansram (3)");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
