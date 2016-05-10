204e51533dc669d7376742fea385c52a5c4b1611
Nicolas San Jose <nas2375@rit.edu>
2016-05-09 23:13:16 -0400

    add waterframes, mats, prefabs, energy shield scripts

---
+++ PolyRunner/Assets/Ship/Scripts/EnergyShieldInstance.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyShieldInstance : MonoBehaviour {
+
+    public float tempA = 0.15f;
+    public float maximumA = 0.3f;
+    public float timer = 0f;
+    public float maxTime = 1f;
+    private float startTime;
+    public float swingRate = 0.05f;
+    
+    void Start () {
+        startTime = Time.time;
+	}
+	void Update () {
+        swingAlpha();
+        GetComponent<MeshRenderer>().material.color = new Color(GetComponent<MeshRenderer>().material.color.r, GetComponent<MeshRenderer>().material.color.g, GetComponent<MeshRenderer>().material.color.b, tempA);
+    }
+    
+    void swingAlpha()
+    {
+        timer = Time.time - startTime;
+        if (tempA < maximumA) {
+            tempA += swingRate;
+        } else {
+            tempA -= swingRate;
+        }
+
+        if (tempA < 0.15f) {
+            Destroy(gameObject);
+        }
+    }
+}
