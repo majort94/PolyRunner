2ac078eda92d8137bea3352d362ca025afe03129
Nicolas San Jose <nas2375@rit.edu>
2016-05-11 14:59:45 -0400

    fixed energy shield anim to handle multiple parts in one ship

--- PolyRunner/Assets/Ship/Scripts/EnergyShieldInstance.cs
+++ PolyRunner/Assets/Ship/Scripts/EnergyShieldInstance.cs
@@ -5,29 +5,42 @@ public class EnergyShieldInstance : MonoBehaviour {
 
     public float tempA = 0.15f;
     public float maximumA = 0.3f;
-    public float timer = 0f;
+    private float timer = 0f;
     public float maxTime = 1f;
     private float startTime;
     public float swingRate = 0.05f;
+    private Component[] shipPieces;
     
     void Start () {
         startTime = Time.time;
-	}
-	void Update () {
-        swingAlpha();
-        GetComponent<MeshRenderer>().material.color = new Color(GetComponent<MeshRenderer>().material.color.r, GetComponent<MeshRenderer>().material.color.g, GetComponent<MeshRenderer>().material.color.b, tempA);
+        shipPieces = GetComponentsInChildren<MeshRenderer>();
     }
-    
-    void swingAlpha()
+
+	void Update () {
+        //Prepare Animation
+        shieldAnimation();
+        //Apply Animation
+        foreach (MeshRenderer shipPiece in shipPieces) {
+            shipPiece.material.color = new Color(GetComponent<MeshRenderer>().material.color.r, GetComponent<MeshRenderer>().material.color.g, GetComponent<MeshRenderer>().material.color.b, tempA);
+        }
+        //Check End State
+        endAnim();
+    }
+
+    void shieldAnimation()
     {
         timer = Time.time - startTime;
         if (tempA < maximumA) {
             tempA += swingRate;
         } else {
             tempA -= swingRate;
-        }
+        }        
+    }
 
-        if (tempA < 0.15f) {
+    void endAnim()
+    {
+        if (tempA < 0.15f)
+        {
             Destroy(gameObject);
         }
     }
